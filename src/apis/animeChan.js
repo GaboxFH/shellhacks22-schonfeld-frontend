7ae2d14dfd77edc790234cd621217162b34b5f30
Gabe <gabefernandez@ufl.edu>
2022-09-10 22:37:38 -0400

    Updated rendered table

--- src/apis/animeChan.js
+++ src/apis/animeChan.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 export const requestQuotes = _.memoize(async title => {
     const res = await fetch(`https://shellhacks22-schonfeld.herokuapp.com/api/securities/${title}`)
+    // const res = await fetch(`http://localhost:8000/api/securities/${title}`)
     if(res.status !== 200) return [];
 
     const quotesArray = await res.json();
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,22 +3,43 @@ import React from 'react';
 import '../styles/Quote.css';
 
 const Quote = ({quote}) => {
-    return (
-        <div className='quote-container'>
-          <p className="quote"> Security id:
-            "{quote.security_id
-}"
-          </p>
-          <p className="quote-author">
-          cusip:
-            <span className='highlight'> {quote.cusip} </span>
-          </p>
-          <p className="quote-author">
-          sedol:
-            <span className='highlight'> {quote.sedol} </span>
-          </p>
-        </div>
-      );
+  return (
+      <tr className='quote-table'>
+        <td className="quote">
+          <span className='highlight'> {quote.security_id} </span>
+        </td>
+        <td className="quote-author">
+          <span className='highlight'> {quote.cusip} </span>
+        </td>
+        <td className="quote-author">
+          <span className='highlight'> {quote.sedol} </span>
+        </td>
+        <td className="quote-author">
+          <span className='highlight'> {quote.isin} </span>
+        </td>
+        <td className="quote-author">
+          <span className='highlight'> {quote.ric} </span>
+        </td>
+        <td className="quote-author">
+          <span className='highlight'> {quote.bloomberg} </span>
+        </td>
+        <td className="quote-author">
+          <span className='highlight'> {quote.bbg} </span>
+        </td>
+        <td className="quote-author">
+          <span className='highlight'> {quote.symbol} </span>
+        </td>
+        <td className="quote-author">
+          <span className='highlight'> {quote.root_symbol} </span>
+        </td>
+        <td className="quote-author">
+          <span className='highlight'> {quote.bb_yellow} </span>
+        </td>
+        <td className="quote-author">
+          <span className='highlight'> {quote.spn} </span>
+        </td>
+      </tr>
+    );
 };
-
+    
 export default Quote;
